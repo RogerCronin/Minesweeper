830969713d739e774ef0e565099a802cfb5d0a5b
Roger Cronin <roger.m.cronin@gmail.com>
2024-03-28 12:36:31 -0400

    add back breaking code

--- script.js
+++ script.js
@@ -81,6 +81,43 @@ function playFormingAnimation(cell) {
     });
 }
 
+function playBreakingAnimation(cell) {
+    const bounds = cell.getBoundingClientRect();
+
+    const pieceContainer = document.createElement("div");
+    pieceContainer.classList.add("broken-piece-container");
+    pieceContainer.style.top = `${bounds.top}px`;
+    pieceContainer.style.left = `${bounds.left}px`;
+
+    const pieces = [];
+    for(let i = 0; i < 4; i++) {
+        const piece = document.createElement("div");
+        piece.classList.add("broken-piece");
+        pieces.push(piece);
+        pieceContainer.appendChild(piece);
+    }
+
+    pieces[0].classList.add("left");
+    pieces[1].classList.add("top");
+    pieces[2].classList.add("right");
+    pieces[3].classList.add("bottom");
+
+    setTimeout(() => {
+        pieces.forEach(p => p.style.opacity = 0);
+
+        pieces[0].style.transform = `translate(-${Math.random() * (cellSize / 2) + (cellSize / 2)}px, ${Math.random() * cellSize - (cellSize / 2)}px) rotate(${Math.random() * 45 * (Math.random() < 0.5 ? 1 : 1)}deg)`;
+        pieces[1].style.transform = `translate(${Math.random() * cellSize - (cellSize / 2)}px, -${Math.random() * (cellSize / 2) + (cellSize / 2)}px) rotate(${Math.random() * 45 * (Math.random() < 0.5 ? 1 : 1)}deg)`;
+        pieces[2].style.transform = `translate(${Math.random() * (cellSize / 2) + (cellSize / 2)}px, ${Math.random() * cellSize - (cellSize / 2)}px) rotate(${Math.random() * 45 * (Math.random() < 0.5 ? 1 : 1)}deg)`;
+        pieces[3].style.transform = `translate(${Math.random() * cellSize - (cellSize / 2)}px, ${Math.random() * (cellSize / 2) + (cellSize / 2)}px) rotate(${Math.random() * 45 * (Math.random() < 0.5 ? 1 : 1)}deg)`;
+
+        pieces[0].addEventListener("transitionend", () => {
+            pieces.forEach(p => p.remove());
+        });
+    }, 1);
+
+    content.appendChild(pieceContainer);
+}
+
 function clickCell(row, column) {
     const cell = cells[row][column];
 
