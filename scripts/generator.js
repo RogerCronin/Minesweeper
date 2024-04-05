7cf0a1786fb58cf7c9e568f401e7f2da57503c76
Roger Cronin <roger.m.cronin@gmail.com>
2024-04-05 12:24:27 -0400

    waofja

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,70 +1,83 @@
 function seedBombsDeterministically(startRow, startColumn) {
+    seedBombsRandomly(startRow, startColumn);
     solveBoard(startRow, startColumn);
 }
 
-function solverGetNumberOfUnknownNeighbors(solverCells, row, column) {
-    let neighborUnknowns = 0;
+function solverGetNeighboringBombs(solverCells, row, column) {
+    let neighboringBombs = 0;
     forAllNeighbors(row, column, neighborCell => {
-        if(solverCells[neighborCell.row][neighborCell.column] === -2) neighborUnknowns++;
+        if(solverCells[neighborCell.row][neighborCell.column].value === -1) neighboringBombs++;
     });
-    return neighborUnknowns;
-}
-
-function solverGetKnownBombNeighbors(solverCells, row, column) {
-    let neighborBombs = 0;
-    forAllNeighbors(row, column, neighborCell => {
-        if(solverCells[neighborCell.row][neighborCell.column] === -1) neighborBombs++;
-    });
-    return neighborBombs;
-}
-
-function solverFlagAllUnknownNeighborsAsBombs(solverCells, row, column, todo) {
-    forAllNeighbors(row, column, neighborCell => {
-        if(solverCells[neighborCell.row][neighborCell.column] === -2) flagCell(solverCells, neighborCell.row, neighborCell.column, todo);
-    });
-}
-
-function solverOpenAllUnknownNeighbors(solverCells, row, column, todo) {
-    forAllNeighbors(row, column, neighborCell => {
-        if(solverCells[neighborCell.row][neighborCell.column] === -2) solverOpenCell(solverCells, neighborCell.row, neighborCell.column, todo);
-    });
+    return neighboringBombs;
 }
 
 function solverOpenCell(solverCells, row, column, todo) {
-    solverCells[row][column] = getNeighboringBombs({ row: row, column: column });
-    forAllNeighbors(row, column, neighborCell => todo.push([neighborCell.row, neighborCell.column]));
+    const cell = solverCells[row][column];
+    if(solverCells.value !== -2) return;
+    cell.value = getNeighboringBombs({ row: row, column: column });
+    cell.constraints.clear();
+    forAllNeighbors(row, column, neighborCell => todo.push([neighborCell.row, neighborCell.column].toString()));
 }
 
 function solverFlagCell(solverCells, row, column, todo) {
-    solverCells[row][column] = -1;
-    forAllNeighbors(row, column, neighborCell => todo.push([neighborCell.row, neighborCell.column]));
+    const cell = solverCells[row][column];
+    if(solverCells.value !== -2) return;
+    cell.value = -1;
+    cell.constraints.clear();
+    forAllNeighbors(row, column, neighborCell => todo.push([neighborCell.row, neighborCell.column].toString()));
 }
 
 function solveBoard(startRow, startColumn) {
-    const solverCells = []; // -2 unknown, -1 confirmed bomb, 0 >= confirmed bomb count
+    const solverCells = [];
     for(let row = 0; row < rows; row++) {
         solverCells.push([]);
         for(let column = 0; column < columns; column++) {
-            solverCells[row].push(-2);
+            solverCells[row].push({
+                value: -2, // -2 unknown, -1 confirmed bomb, 0 >= confirmed bomb count
+                constraints: new Set()
+            });
+            forAllNeighbors(row, column, neighborCell => {
+                if(neighborCell.row === row && neighborCell.column === column) return;
+                solverCells[row][solverCells[row].length - 1].constraints.add([neighborCell.row, neighborCell.column].toString());
+            });
         }
     }
 
-    solverCells[startRow][startColumn] = 0;
+    solverCells[startRow][startColumn].value = 0;
     const todo = [[startRow, startColumn]]; // list of (row, column) of cells to process
     while(todo.length !== 0) { // while we still have cells to process
         const [ row, column ] = todo[0];
 
-        const numberOfBombs = solverCells[row][column];
-        const numberOfUnknownNeighbors = solverGetNumberOfUnknownNeighbors(solverCells, row, column);
-        const numberOfKnownBombNeighbors = solverGetKnownBombNeighbors(solverCells, row, column);
+        console.log(`Processing [${row}, ${column}]`);
 
-        // if a cell's number equals number of known bombs, non bombs are guaranteed free
-        if(numberOfKnownBombNeighbors === numberOfBombs) {
-            solverOpenAllUnknownNeighbors(solverCells, row, column, todo);
+        const solverCell = solverCells[row][column];
+
+        // update constraints
+        forAllNeighbors(row, column, neighborCell => {
+            const solverNeighbor = solverCells[neighborCell.row][neighborCell.column];
+            if(solverNeighbor.value === -2) return;
+            solverCell.constraints.delete([neighborCell.row, neighborCell.column].toString()); // remove from constraints
+        });
+
+        console.log(`Value: ${solverCell.value}, Constraints Size: ${solverCell.constraints.size}, Neighboring Bombs: ${solverGetNeighboringBombs(solverCells, row, column)}`);
+
+        // numer of bombs is equal to number of neighbors
+        if(solverCell.value === solverCell.constraints.size) {
+            console.log("All constraints are bombs");
+            for(const neighborCell of solverCell.constraints) {
+                const coords = JSON.parse(`[${neighborCell}]`);
+                solverFlagCell(solverCells, coords[0], coords[1]);
+            }
+            solverCell.constraints.clear();
         }
-        // if a cell's number equals number of unopened squares, they are guaranteed bombs
-        if(numberOfUnknownNeighbors === numberOfBombs - numberOfKnownBombNeighbors) {
-            solverFlagAllUnknownNeighborsAsBombs(solverCells, row, column, todo);
+        
+        // number of bombs is equal to number of neighbor flags
+        if(solverCell.value === solverGetNeighboringBombs(solverCells, row, column)) {
+            console.log("All constraints are safe");
+            for(const neighborCell of solverCell.constraints) {
+                const coords = JSON.parse(`[${neighborCell}]`);
+                solverOpenCell(solverCells, coords[0], coords[1]);
+            }
         }
 
         todo.shift(); // done processing this cell
@@ -73,8 +86,8 @@ function solveBoard(startRow, startColumn) {
     // update onto screen our progress (for now lol)
     for(let row = 0; row < rows; row++) {
         for(let column = 0; column < columns; column++) {
-            if(solverCells[row][column] === -1) toggleFlagForCell(row, column);
-            else if(solverCells[row][column] >= 0) clickCell(row, column);
+            if(solverCells[row][column].value === -1) toggleFlagForCell(row, column);
+            else if(solverCells[row][column].value >= 0) clickCell(row, column);
         }
     }
 }
@@ -94,4 +107,7 @@ function seedBombsRandomly(startRow, startColumn) {
     }
 }
 
-let seedBombs = seedBombsDeterministically;
+let seedBombs = (startRow, startColumn) => {
+    firstClick = false;
+    seedBombsDeterministically(startRow, startColumn);
+}
